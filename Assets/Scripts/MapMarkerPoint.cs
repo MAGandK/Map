2a331220eff4af5dfe972e3875b66b878da12bbe
AnnaMigitenko <migitenko@gmail.com>
2024-09-04 15:28:25 +0300

    added scale

--- Assets/Scripts/MapMarkerPoint.cs
+++ Assets/Scripts/MapMarkerPoint.cs
@@ -8,11 +8,25 @@ public class MapMarkerPoint : MonoBehaviour
    private float _mapWidth = 500f; 
    private float _mapHeight = 500f; 
 
+   public RectTransform MapMain;
    public RectTransform MapMarker;
    public Transform PointPosition;
 
+   
+   private float _mapScale = 1f;
+   private float _minScale = 1f; 
+   private float _maxScale = 5f;
    private void Update()
    {
+      float scroll = Input.GetAxis("Mouse ScrollWheel");
+      if (scroll != 0)
+      {
+         _mapScale = Mathf.Clamp(_mapScale + scroll, _minScale, _maxScale);
+         UpdateMapSize();
+         FindMarkerPosition();
+      }
+
+      
       if (Input.GetKeyDown(KeyCode.A))
       {
          FindMarkerPosition();
@@ -21,7 +35,13 @@ public class MapMarkerPoint : MonoBehaviour
 
    public void FindMarkerPosition()
    {
-      Vector2 marketPos = new Vector2(PointPosition.position.x * 0.3f, -PointPosition.position.z * 0.3f);
-      MapMarker.anchoredPosition = new Vector2(marketPos.x, marketPos.y);
+      Vector2 markerPos = new Vector2(PointPosition.position.x * 0.3f * _mapScale, -PointPosition.position.z * 0.3f * _mapScale);
+      MapMarker.anchoredPosition = markerPos;
+
+   }
+   
+   private void UpdateMapSize()
+   { 
+     MapMain.sizeDelta = new Vector2(_mapWidth * _mapScale, _mapHeight * _mapScale); 
    }
 }
